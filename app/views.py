4c4b1a9c3c3f0bd529a57169905caa2cd86899bf
Vitor Moura <jvrezendemoura@gmail.com>
2024-07-29 19:42:48 -0300

    Adding login requiriment to the site

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from functools import wraps
 import logging
 from datetime import datetime, date, time
 from .utils import Log
+
 views = Blueprint("views", __name__)
 
 def login_required(f):
@@ -14,7 +15,7 @@ def login_required(f):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash("You need to login first", 'alert')
+            flash("You need to login first", category='alert')
             return redirect( url_for("views.index"))
     return wrap
 
@@ -29,12 +30,11 @@ def signup():
     form = SignUpForm()
     if request.method == 'GET':
         return render_template('signup.jinja', form = form)
+    
     elif request.method == "POST":
         
-        # TODO: fazer com que envie uma mensagem dizendo que o osuuário ja existe
-        #flash("Esse usuário ja existe dentro da base de dados")
-        #return redirect(url_for('views.index'))    
         if form.validate_on_submit():
+    
             if User.query.filter_by(email=request.form["email"]).first() is None:
                 Log.cadastro_de_usuario(request)
 
@@ -45,50 +45,68 @@ def signup():
                 )
                 db.session.add(user)
                 db.session.commit()
-
-
+                    
+                session['logged_in'] = True
                 return redirect(url_for('views.homepage'))
             
             else:
                 user = User.query.filter_by(email=request.form['email']).first()
 
-            flash("As credenciais já foram cadastradas no site")
+            flash("As credenciais já foram cadastradas no site", category="alert")
             return redirect(url_for('views.index'))
         
 
 
-@views.route("/login")
+@views.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.jinja")
 
+    if request.method == "GET":
+        return render_template("login.jinja")
+    
+    else:
+        if form.validate_on_submit():
+            email = request.form["email"]
+            password = request.form["password"]
+            session['logged_in'] = True
+
+            if user := User.query.filter_by(email=email).first() is not None:
+                print(user)                
 
 @views.route("/homepage")
+@login_required
 def homepage():
+    print(session['logged_in'])
     return render_template('homepage.jinja')
 
 
 
 @views.route("/tables")
+@login_required 
 def tables():
     return "Tables"
 
 @views.route("/games")
+@login_required
 def games():
     return "games"
 
 
 @views.route("/private_games")
+@login_required
 def private_games():
     return "private_games"
 
 
-@views.route("/profile/")
+@views.route("/profile")
+@login_required
 def profile():
     if request.method == "GET":
         return render_template("profile.jinja")
     
 
 @views.route("/logout")
+@login_required
 def logout():
-    return "logout"
+    session.pop("logged_in", None)
+    return redirect(url_for("views.index"))
     
